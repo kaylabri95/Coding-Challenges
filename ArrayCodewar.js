45716816a894fa9c9cd018308b7b7579defa0113
Kayla Burns <kaylabri95@gmail.com>
2022-09-11 21:06:51 -0400

    Daily Codewar

---
+++ ArrayCodewar.js
@@ -0,0 +1,8 @@
+// This shows you how to produce an array of every number leading up to the number input.
+function arr(n){
+    var newArr = [];
+    for(var i = 0; i < n; i++){
+      newArr.push(i);
+    }
+    return newArr;
+  }
