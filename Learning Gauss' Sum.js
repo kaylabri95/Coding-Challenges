0f184008d99a6da9279366a19668a15507e4d014
kaylabri95 <kaylabri95@gmail.com>
2022-10-12 20:00:00 -0600

    Daily codewar

---
+++ Learning Gauss' Sum.js
@@ -0,0 +1,9 @@
+// Learning Gauss' Sum
+
+let ary = [1,2,3,4,5,6]
+
+let sumContiguousArray = function(ary){
+    const lastItem = ary[ary.length - 1]
+    return lastItem, * (lastItem + 1) / 2
+}
+// This is so cool. Way to find the sum without leaving constant time O(1) and going into O(n)
